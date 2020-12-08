65fd9608be9331606bb13182d3de927f42b53db4
Søren Heltborg Kristensen <62941288+sorenheltborgkristensen@users.noreply.github.com>
2020-12-08 15:57:36 +0100

    currency converter

--- scripts/main.js
+++ scripts/main.js
@@ -4,47 +4,46 @@ const amount = document.querySelector(".amount");
 const result = document.querySelector(".result");
 const swap = document.querySelector(".swap");
 
-const getCurrencyNames = async () => {
-  const response = await fetch("https://api.frankfurter.app/currencies");
+const url_currencies = "https://api.frankfurter.app/currencies";
+const url_conversion = "https://api.frankfurter.app/latest";
+
+const cunrrencies = async () => {
+  const response = await fetch(url_currencies);
   const data = await response.json();
   const currencies = Object.entries(data);
-
-  const option = currencies.map((currency) => `<option value="${currency[0]}">${currency[1]}</option>`).join("\n");
-
-  from.innerHTML = option;
-  to.innerHTML = option;
-
-  from.value = "EUR";
-  to.value = "DKK";
-  amount.value = 1;
-
-  convertCurrency();
+  return currencies;
 };
 
-const convertCurrency = async () => {
+const conversion = async () => {
   if (from.value === to.value) {
     result.textContent = amount.value;
-  } else {
-    const response = await fetch(`https://api.frankfurter.app/latest?amount=${amount.value}&from=${from.value}&to=${to.value}`);
+  } else if (amount.value.length !== 0 && from.value.length !== 0 && to.value.length !== 0) {
+    const response = await fetch(url_conversion + `?amount=${amount.value}&from=${from.value}&to=${to.value}`);
     const data = await response.json();
-    const conversionResult = Object.values(data.rates);
-
-    result.textContent = parseFloat(conversionResult).toFixed(2);
+    const conversion = Object.values(data.rates);
+    result.textContent = parseFloat(conversion).toFixed(2);
   }
 };
 
-const swapCurrency = () => {
+cunrrencies().then((currencies) => {
+  const option = currencies.map((currency) => `<option value="${currency[0]}">${currency[0]} - ${currency[1]}</option>`).join("\n");
+  from.innerHTML = option;
+  to.innerHTML = option;
+  from.value = "EUR";
+  to.value = "DKK";
+  amount.value = 1;
+  conversion();
+});
+
+const swapCurrencies = () => {
   temp = from.value;
   from.value = to.value;
   to.value = temp;
-  convertCurrency();
+  conversion();
 };
 
-from.addEventListener("change", convertCurrency);
-to.addEventListener("change", convertCurrency);
-amount.addEventListener("change", convertCurrency);
-amount.addEventListener("keyup", convertCurrency);
-swap.addEventListener("click", swapCurrency);
-
-getCurrencyNames();
-convertCurrency();
+from.addEventListener("change", conversion);
+to.addEventListener("change", conversion);
+amount.addEventListener("change", conversion);
+amount.addEventListener("keyup", conversion);
+swap.addEventListener("click", swapCurrencies);
